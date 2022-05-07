e420cdefe5297d67bc1724447b38b7fe41cdb099
Fe3O4-Git <Fe3O4_Live@Outlook.com>
2022-05-07 09:50:55 +0800

    format code

--- app/src/main/java/org/element/gotopc/activities/BaseActivity.java
+++ app/src/main/java/org/element/gotopc/activities/BaseActivity.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
